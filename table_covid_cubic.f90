dabf005cb503b6b9234b1ab9009db35bd9a670f0
Gustavo <gustavoquintero9105@gmail.com>
2023-09-20 00:17:14 -0300

    Updated on 09-20-2023 at 00:17

--- table_covid_cubic.f90
+++ table_covid_cubic.f90
@@ -3,18 +3,21 @@ program table
 
     real(kind=8) ::  y_true,y_pred,error
     real(kind=8), allocatable :: train_set(:),test_set(:),x(:),accuracy(:,:)
-    integer :: i,allocerr,n_train,n_test
+    integer :: i,j,allocerr,n_train,n_test,inf
 
     ! Reading data and storing it in the variables t and y
     Open(Unit = 100, File = "output/covid_train.txt", ACCESS = "SEQUENTIAL")
     Open(Unit = 200, File = "output/covid_test.txt", ACCESS = "SEQUENTIAL")
     Open(Unit = 300, File = "output/solutions_covid_cubic.txt", ACCESS = "SEQUENTIAL")
+    Open(Unit = 400, File = "output/accuracy_matrix.txt", ACCESS = "SEQUENTIAL")
 
     ! Set parameters
     read(100,*) n_train
     read(200,*) n_test
 
-    allocate(train_set(n_train),test_set(n_test),x(3),accuracy(n_train-4,n_test),stat=allocerr)
+    inf = 5
+
+    allocate(train_set(n_train),test_set(n_test),x(3),accuracy(n_train - inf + 1,n_test),stat=allocerr)
 
     if ( allocerr .ne. 0 ) then
         write(*,*) 'Allocation error.'
@@ -29,19 +32,22 @@ program table
         read(200,*) test_set(i)
     enddo
 
-    read(300,*) x
-
     close(100)
     close(200)
 
-    y_true = test_set(1)
-    call cubic_model(x,6,5,train_set(n_train),3,y_pred)
+    do i = 1, n_train - inf + 1
+        read(300,*) x
+        do j = 1, n_test
+            y_true = test_set(j)
+            call cubic_model(x,inf+i+j-1,inf+i-1,train_set(n_train),3,y_pred)
+            call percentage_error(y_true,y_pred,accuracy(i,j))
+            write(400,*) accuracy(i,:)
+        enddo
+    enddo
 
-    call percentage_error(y_true,y_pred,error)
-
-    print*, y_true,y_pred,error
-
-
+    close(300)
+    close(400)
+    
     contains
 
     subroutine cubic_model(x,t,tm,ym,n,res)
