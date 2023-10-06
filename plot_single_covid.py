aaa59e87d5f53d613951ee84e7f73ade561262ce
Gustavo <gustavoquintero9105@gmail.com>
2023-10-05 19:15:59 -0500

    Updated on 10-05-2023 at 19:15

--- plot_single_covid.py
+++ plot_single_covid.py
@@ -3,8 +3,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import models
 
-def plot_models(opt):
-    return 0
+def plot_models(opt=3):
+    plt.plot(days,y[n_train - previous_days:],"ko")
+    plt.plot(days_later,y_later,"o")
+
+    if opt == 1:
+        plt.plot(t,models.cubic(*x_cubic,t,y[-1],days[-1]))
+        plt.plot(outliers[0,0,:],outliers[0,1,:],'ro',mfc='none',ms=10)
+
+    elif opt == 2:
+        plt.plot(t,models.logistic(*x_logistic,t))
+        plt.plot(outliers[1,0,:],outliers[1,1,:],'ro',mfc='none',ms=10)
+
+    else:
+        plt.plot(t,models.cubic(*x_cubic,t,y[-1],days[-1]))
+        plt.plot(t,models.logistic(*x_logistic,t))
+
+    plt.show()
 
 # Reading the necessary data
 df_solution_cubic       = pd.read_table("output/solutions_covid_cubic.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
@@ -52,34 +67,45 @@ with open("output/outliers_covid_cubic.txt") as f:
     lines = f.readlines()
     xdata = [line.split()[0] for line in lines]
 
-noutliers = int(xdata[0])
-
-ind_outliers = np.empty(noutliers,dtype=int)
+noutliers       = int(xdata[0])
+outliers        = np.empty((2,2,noutliers))
+ind_outliers    = np.empty(noutliers,dtype=int)
 
 for i in range(noutliers):
     ind_outliers[i] = int(xdata[i+1])
 
-outliers = np.empty((2,noutliers))
+for i in range(noutliers):
+    outliers[0,0,i] = days[ind_outliers[i]-1]
+    outliers[0,1,i] = y[n_train - previous_days + ind_outliers[i]-1]
+
+with open("output/outliers_covid_logistic.txt") as f:
+    lines = f.readlines()
+    xdata = [line.split()[0] for line in lines]
 
 for i in range(noutliers):
-    outliers[0,i] = days[ind_outliers[i]-1]
-    outliers[1,i] = y[n_train - previous_days + ind_outliers[i]-1]
+    ind_outliers[i] = int(xdata[i+1])
 
-plt.plot(days,y[n_train - previous_days:],"ko")
-plt.plot(days_later,y_later,"o")
-# plt.plot(t,models.cubic(*x,t,y[-1],days[-1]))
-plt.plot(t,models.logistic(*x,t))
-plt.plot(outliers[0],outliers[1],'ro',mfc='none',ms=10)
+for i in range(noutliers):
+    outliers[1,0,i] = days[ind_outliers[i]-1]
+    outliers[1,1,i] = y[n_train - previous_days + ind_outliers[i]-1]
 
-# for i in range(noutliers):
-#     point1 = [outliers[0,i],cubic(*x,outliers[0,i],y[-1],t[-1])]
-#     point2 = [outliers[0,i],outliers[1,i]]
-#     x_values = [point1[0], point2[0]]
-#     y_values = [point1[1], point2[1]]
-#     plt.plot(x_values, y_values, 'k', linestyle="--")
+plot_models()
 
-# l = plt.plot(day,y[n_train-20:],"ko")
-# plt.setp(l, 'markersize', 6)
+# plt.plot(days,y[n_train - previous_days:],"ko")
+# plt.plot(days_later,y_later,"o")
+# # plt.plot(t,models.cubic(*x,t,y[-1],days[-1]))
+# plt.plot(t,models.logistic(*x,t))
+# plt.plot(outliers[0],outliers[1],'ro',mfc='none',ms=10)
 
-plt.savefig("images/single_covid.pdf",bbox_inches = "tight")
-plt.show()
+# # for i in range(noutliers):
+# #     point1 = [outliers[0,i],cubic(*x,outliers[0,i],y[-1],t[-1])]
+# #     point2 = [outliers[0,i],outliers[1,i]]
+# #     x_values = [point1[0], point2[0]]
+# #     y_values = [point1[1], point2[1]]
+# #     plt.plot(x_values, y_values, 'k', linestyle="--")
+
+# # l = plt.plot(day,y[n_train-20:],"ko")
+# # plt.setp(l, 'markersize', 6)
+
+# plt.savefig("images/single_covid.pdf",bbox_inches = "tight")
+# plt.show()
