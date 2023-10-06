d7152d3f5c10bd91ce0413104d846ae1586c2c02
Gustavo <gustavoquintero9105@gmail.com>
2023-10-05 21:58:26 -0500

    Updated on 10-05-2023 at 21:58

--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 import numpy as np
 
 # Farrington models
-l = lambda t, a, b, c: (a * t - c) * np.exp(-b * t) + c
+def l(t,a,b,c): 
+    return (a * t - c) * np.exp(-b * t) + c
 
-F = lambda t, a, b, c: 1.0 - np.exp((a/b) * t * np.exp(-b * t) + (1.0/b) * ((a/b) - c) * \
-    (np.exp(-b * t) - 1.0) - c * t)
+def F(t,a,b,c):
+    return 1.0 - np.exp((a/b) * t * np.exp(-b * t) + \
+    (1.0/b) * ((a/b) - c) * (np.exp(-b * t) - 1.0) - c * t)
 
 # Covid models
-cubic = lambda x1,x2,x3,t,ym,tm: ym + x1 * (t - tm) + x2 * (t - tm)**2 + x3 * (t - tm)**3
+def cubic(x1,x2,x3,t,ym,tm):
+    return ym + x1 * (t - tm) + x2 * (t - tm)**2 + x3 * (t - tm)**3
 
-logistic = lambda x1,x2,x3,t: x1 * x3 * np.exp(x2 * t) / (x3 + x1 * (np.exp(x2 * t) - 1.0))
+def logistic(x1,x2,x3,t):
+    return x1 * x3 * np.exp(x2 * t) / (x3 + x1 * (np.exp(x2 * t) - 1.0))
+
+def der_logistic(x1,x2,x3,t):
+    return x1 * x2 * x3 * (x3 - x1) * np.exp(x2 * t) / \
+    (x3 + x1 * (np.exp(x2 * t) - 1.0))**3
--- plot_single_covid.py
+++ plot_single_covid.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import models
 
-def plot_models(opt=3):
+def plot_models(opt=None):
     plt.plot(days,y[n_train - previous_days:],"ko")
     plt.plot(days_later,y_later,"o",color="grey")
 
@@ -16,11 +16,16 @@ def plot_models(opt=3):
         plt.plot(t,models.logistic(*x_logistic,t))
         plt.plot(outliers[1,0,:],outliers[1,1,:],'ro',mfc='none',ms=10)
         plt.savefig("images/single_covid_logistic.pdf",bbox_inches = "tight")
+    
+    elif opt == 3:
+        plt.plot(t,models.der_logistic(*x_der_logistic,t))
+        plt.plot(outliers[2,0,:],outliers[2,1,:],'ro',mfc='none',ms=10)
+        plt.savefig("images/single_covid_der_logistic.pdf",bbox_inches = "tight")
 
     else:
         plt.plot(t,models.cubic(*x_cubic,t,y[-1],days[-1]))
         plt.plot(t,models.logistic(*x_logistic,t))
-        plt.savefig("images/covid_cubic_logistic.pdf",bbox_inches = "tight")
+        plt.savefig("images/covid_all_models.pdf",bbox_inches = "tight")
 
     plt.show()
     plt.close()
@@ -28,6 +33,7 @@ def plot_models(opt=3):
 # Reading the necessary data
 df_solution_cubic       = pd.read_table("output/solutions_covid_cubic.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df_solution_logistic    = pd.read_table("output/solutions_covid_logistic.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_solution_der_logistic= pd.read_table("output/solutions_covid_der_logistic.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df_train_set            = pd.read_table("data/covid_train.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df_test_set             = pd.read_table("data/covid_test.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 n_train                 = int(df_train_set.values[0][0])
@@ -41,13 +47,14 @@ with open("output/inf_sup_covid.txt") as f:
 previous_days = int(xdata[0])
 
 # Arrays allocation
-x_cubic     = np.zeros(3)
-x_logistic  = np.zeros(3)
-y           = np.zeros(n_train)
-y_later     = np.zeros(n_test)
-days        = np.linspace(1,previous_days,previous_days)
-days_later  = np.linspace(1 + previous_days,previous_days + n_test,n_test)
-t           = np.linspace(1,previous_days + n_test,1000)
+x_cubic         = np.zeros(3)
+x_logistic      = np.zeros(3)
+x_der_logistic  = np.zeros(3)
+y               = np.zeros(n_train)
+y_later         = np.zeros(n_test)
+days            = np.linspace(1,previous_days,previous_days)
+days_later      = np.linspace(1 + previous_days,previous_days + n_test,n_test)
+t               = np.linspace(1,previous_days + n_test,1000)
 
 # Solution with cubic model
 x_cubic[0] = df_solution_cubic.values[0][0]
@@ -58,6 +65,11 @@ x_cubic[2] = df_solution_cubic.values[0][2]
 x_logistic[0] = df_solution_logistic.values[0][0]
 x_logistic[1] = df_solution_logistic.values[0][1]
 x_logistic[2] = df_solution_logistic.values[0][2]
+
+# Solution with logistic derivative model
+x_der_logistic[0] = df_solution_der_logistic.values[0][0]
+x_der_logistic[1] = df_solution_der_logistic.values[0][1]
+x_der_logistic[2] = df_solution_der_logistic.values[0][2]
 
 # Observation in the previous days considered
 for i in range(n_train):
@@ -72,7 +84,7 @@ with open("output/outliers_covid_cubic.txt") as f:
     xdata = [line.split()[0] for line in lines]
 
 noutliers       = int(xdata[0])
-outliers        = np.empty((2,2,noutliers))
+outliers        = np.empty((3,2,noutliers))
 ind_outliers    = np.empty(noutliers,dtype=int)
 
 for i in range(noutliers):
@@ -93,9 +105,21 @@ for i in range(noutliers):
     outliers[1,0,i] = days[ind_outliers[i]-1]
     outliers[1,1,i] = y[n_train - previous_days + ind_outliers[i]-1]
 
-plot_models(1)
-plot_models(2)
+with open("output/outliers_covid_der_logistic.txt") as f:
+    lines = f.readlines()
+    xdata = [line.split()[0] for line in lines]
+
+for i in range(noutliers):
+    ind_outliers[i] = int(xdata[i+1])
+
+for i in range(noutliers):
+    outliers[2,0,i] = days[ind_outliers[i]-1]
+    outliers[2,1,i] = y[n_train - previous_days + ind_outliers[i]-1]
+
+# plot_models(1)
+# plot_models(2)
 plot_models(3)
+# plot_models()
 
 # plt.plot(days,y[n_train - previous_days:],"ko")
 # plt.plot(days_later,y_later,"o")
