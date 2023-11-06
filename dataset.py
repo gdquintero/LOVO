16ee9eeac239978954ed2134051e4db90bd194a5
Gustavo <gustavoquintero9105@gmail.com>
2023-11-06 16:19:21 -0500

    Updated on 11-06-2023 at 16:19

--- dataset.py
+++ dataset.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
-ind_excel_init = 91
-ind_excel_end = 365
-init_date = ind_excel_init - 2
-end_date = ind_excel_end - 2
+countries = ["ar.xlsx","br.xlsx","co.xlsx","es.xlsx","it.xlsx","pa.xlsx","uk.xlsx","us.xlsx"]
+country = countries[1]
 
-with open("data/brazil","w") as f:
+df = pd.read_excel("data/"+country)
+
+ind_excel_init  = 91
+ind_excel_end   = 365
+init_date       = ind_excel_init - 2
+end_date        = ind_excel_end - 2
+
+with open("data/br.txt","w") as f:
+    for i in range(ind_excel_end-ind_excel_init+1):
+        x = df["new_deaths_smoothed_per_million"][i+init_date]
+
+        if pd.isna(x) == True:
+            f.write("%f\n" % 0.0)
+        else:
+            f.write("%f\n" % x)
