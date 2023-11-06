1a2f4d6aed28fe941ea17b97c24c8ea852ac5c1a
Gustavo <gustavoquintero9105@gmail.com>
2023-11-06 16:21:53 -0500

    Updated on 11-06-2023 at 16:21

--- dataset.py
+++ dataset.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-countries = ["ar.xlsx","br.xlsx","co.xlsx","es.xlsx","it.xlsx","pa.xlsx","uk.xlsx","us.xlsx"]
+countries = ["ar","br","co","es","it","pa","uk","us"]
 country = countries[1]
 
-df = pd.read_excel("data/"+country)
+df = pd.read_excel("data/"+country+".xlsx")
 
 ind_excel_init  = 91
 ind_excel_end   = 365
 init_date       = ind_excel_init - 2
 end_date        = ind_excel_end - 2
 
-with open("data/br.txt","w") as f:
+with open("data/"+country+".txt","w") as f:
     for i in range(ind_excel_end-ind_excel_init+1):
         x = df["new_deaths_smoothed_per_million"][i+init_date]
 
