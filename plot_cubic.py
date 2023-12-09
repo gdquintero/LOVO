54ff1bd4fefff3eef963f3da5a55c29676782864
Gustavo <gustavoquintero9105@gmail.com>
2023-12-09 15:26:41 -0500

    Updated on 12-09-2023 at 15:26

---
+++ plot_cubic.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_table("data/cubic.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+
+plt.plot(df[0].values,df[1].values,"o")
+plt.show()
