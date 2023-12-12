8995dbf7dcd1db4ca805d8669a183a0ec9c66236
Gustavo <gustavoquintero9105@gmail.com>
2023-12-11 21:28:32 -0500

    Updated on 12-11-2023 at 21:28

--- plot_cubic.py
+++ plot_cubic.py
@@ -7,6 +7,13 @@ import matplotlib.colors as colors
 plt.rcParams.update({'font.size': 14})
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
+NUM_COLORS = 11
+cm = plt.get_cmap('rainbow')
+cNorm  = colors.Normalize(vmin=0, vmax=NUM_COLORS-1)
+scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_prop_cycle(color=[scalarMap.to_rgba(NUM_COLORS-i) for i in range(NUM_COLORS)])
 
 def poly(x,t):
     return x[0] + x[1] * t + x[2] * (t**2) + x[3] * (t**3)
@@ -16,7 +23,7 @@ df_sol = pd.read_table("output/solution_cubic.txt",delimiter=" ",header=None,ski
 
 t = np.linspace(df_data[0].values[0],df_data[0].values[-1],1000)
 
-plt.plot(df_data[0].values,df_data[1].values,"o")
+plt.plot(df_data[0].values,df_data[1].values,"ok")
 
 for i in range(len(df_sol)):
     plt.plot(t,poly(df_sol.values[i][:4],t),label="o = " + str(i))
