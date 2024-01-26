4d3e4a9917c9ba35dec73fa0e2579c7c879808e4
Gustavo <gustavoquintero9105@gmail.com>
2024-01-26 09:43:29 -0500

    Updated on 01-26-2024 at 09:43

--- experiment_3/sources/plot_log_sp.py
+++ experiment_3/sources/plot_log_sp.py
@@ -3,6 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+def plot1():
+    fig = plt.figure()
+    ax = fig.subplots(1, 1)
+    ax.loglog(x,y,"ko",ls=":")
+    ax.set_xscale('linear')
+    ax.set_xlim(inf,sup)
+    ax.set_ylim(min(np.log10(y)),max(np.log10(y))+10)
+    plt.show()
+
+def plot2():
+    fig = plt.figure()
+    ax = fig.subplots(1, 1)
+    ax.semilogx(x,y)
+    ax.set_yscale('linear')
+    plt.show()
+
 cwd = os.getcwd()
 parent =  os.path.abspath(os.path.join(cwd,os.pardir))
 
@@ -19,15 +35,7 @@ with open(parent+"/output/num_mixed_test.txt") as f:
 inf = int(lim[0])
 sup = int(lim[1])
 n = sup - inf + 1
+x = np.linspace(inf,sup,n)
+y = df_sp[0].values
 
-t = np.linspace(inf,sup,n)
-
-fig = plt.figure()
-ax = fig.subplots(1, 1)
-ax.loglog(t,df_sp[0].values,"ko",ls=":")
-ax.set_xscale('linear')
-ax.set_xlim(inf-0.5,sup+0.5)
-ax.set_ylim(min(np.log10(df_sp[0].values)),max(np.log10(df_sp[0].values))+10)
-
-plt.show()
-
+plot2()
