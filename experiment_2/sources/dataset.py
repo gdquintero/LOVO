d5ee6e0349b628ad277ba8cbac4755104a9b9f88
Gustavo <gustavoquintero9105@gmail.com>
2024-02-10 17:11:55 -0500

    Updated on 02-10-2024 at 17:11

--- experiment_2/sources/dataset.py
+++ experiment_2/sources/dataset.py
@@ -1,17 +1,23 @@
 import pandas as pd
+import os
 
 countries = ["ar","br","co","es","it","pa","uk","us"]
 country = countries[1]
 
-df = pd.read_excel("data/"+country+".xlsx")
+cwd = os.getcwd()
+parent =  os.path.abspath(os.path.join(cwd,os.pardir))
+
+df = pd.read_excel(parent+"/data/"+country+".xlsx")
+
 
 ind_excel_init  = 91 # 01-04-2020
 ind_excel_end   = 365 # 31-12-2020
 init_date       = ind_excel_init - 2
 end_date        = ind_excel_end - 2
-n = ind_excel_end-ind_excel_init+1
+n = ind_excel_end-ind_excel_init + 1
 
-with open("data/covid.txt","w") as f:
+
+with open(parent+"/data/covid.txt","w") as f:
     f.write("%i\n" % n)
     for i in range(n):
         x = df["new_deaths_smoothed_per_million"][i+init_date]
