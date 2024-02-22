e3cd61e09757a2168c4abbbe556d8b8620e967aa
Gustavo <gustavoquintero9105@gmail.com>
2024-02-22 05:06:24 -0500

    Updated on 02-22-2024 at 05:06

---
+++ experiment_2/sources/compute_mtrain_mtest.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import os
+
+cwd = os.getcwd()
+parent =  os.path.abspath(os.path.join(cwd,os.pardir))
+
+df_error_10 = pd.read_table(parent+"/output/latex_5.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_error_20 = pd.read_table(parent+"/output/latex_10.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_error_30 = pd.read_table(parent+"/output/latex_15.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_error_10 = pd.read_table(parent+"/output/latex_20.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_error_20 = pd.read_table(parent+"/output/latex_25.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_error_30 = pd.read_table(parent+"/output/latex_30.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+
--- experiment_2/sources/export.py
+++ experiment_2/sources/export.py
@@ -5,20 +5,38 @@ import os
 cwd = os.getcwd()
 parent =  os.path.abspath(os.path.join(cwd,os.pardir))
 
+df= pd.read_table(parent+"/output/latex_5.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df.to_excel(parent+"/output/latex_5.xlsx")
+
 df= pd.read_table(parent+"/output/latex_10.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df.to_excel(parent+"/output/latex_10.xlsx")
+
+df= pd.read_table(parent+"/output/latex_15.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df.to_excel(parent+"/output/latex_15.xlsx")
 
 df= pd.read_table(parent+"/output/latex_20.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df.to_excel(parent+"/output/latex_20.xlsx")
 
+df= pd.read_table(parent+"/output/latex_25.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df.to_excel(parent+"/output/latex_25.xlsx")
+
 df= pd.read_table(parent+"/output/latex_30.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df.to_excel(parent+"/output/latex_30.xlsx")
 
-df= pd.read_table(parent+"/output/mean_re_10.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
-df.to_excel(parent+"/output/mean_re_10.xlsx")
+# df= pd.read_table(parent+"/output/mean_re_5.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_10.xlsx")
 
-df= pd.read_table(parent+"/output/mean_re_20.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
-df.to_excel(parent+"/output/mean_re_20.xlsx")
+# df= pd.read_table(parent+"/output/mean_re_10.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_20.xlsx")
 
-df= pd.read_table(parent+"/output/mean_re_30.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
-df.to_excel(parent+"/output/mean_re_30.xlsx")
+# df= pd.read_table(parent+"/output/mean_re_15.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_30.xlsx")
+
+# df= pd.read_table(parent+"/output/mean_re_20.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_10.xlsx")
+
+# df= pd.read_table(parent+"/output/mean_re_25.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_20.xlsx")
+
+# df= pd.read_table(parent+"/output/mean_re_30.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+# df.to_excel(parent+"/output/mean_re_30.xlsx")
