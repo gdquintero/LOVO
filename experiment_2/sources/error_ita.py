49f638a4404abbb40045f7219835881c778a3fe4
Gustavo Quintero <gustavoquintero@MacBook-Pro-de-Gustavo-2.local>
2024-04-20 20:33:55 -0500

    Updated on 04-20-2024 at 20:33

--- experiment_2/sources/error_ita.py
+++ experiment_2/sources/error_ita.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 cwd = os.getcwd()
 parent =  os.path.abspath(os.path.join(cwd,os.pardir))
+population = 41128772
 
 def rmsd (n,o,p):
 
@@ -19,6 +20,9 @@ def rmsd (n,o,p):
 
 def relative_error(o,p):
     return abs(p - o) / abs(o)
+
+def absolute_error(o,p):
+    return abs(o-p)
 
 df_data = pd.read_table(parent+"/data/covid.txt",delimiter=" ",header=None,skiprows=2,skipinitialspace=True)
 df_sol = pd.read_table(parent+"/output/solution_covid.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
@@ -35,15 +39,18 @@ for i in range(3):
     pred[i] = models.cubic(x[0],x[1],x[2],tm+i+1,ym,tm)
     obs[i] = df_data[0].values[i]
     
-errors[:] = relative_error(obs,pred)
+errors[:] = absolute_error(obs,pred)
 
-print("Relative errors: ")
+print("Obs: ",obs)
+print("pred: ",pred)
+
+print("\nAbsolute errors: ")
 for i in range(3):
     print("%.4f" % errors[i])
 
-print("\nMean RE: ")
+print("\nMean AE: ")
 print("%.4f" % st.mean(errors))
 
-print("\nRMSD: ")
-print("%.4f" % rmsd(3,obs,pred))
+# print("\nRMSD: ")
+# print("%.4f" % rmsd(3,obs,pred))
 
--- experiment_2/sources/error_measures.py
+++ experiment_2/sources/error_measures.py
@@ -26,6 +26,9 @@ def rmsd (n,o,p):
 def relative_error(o,p):
     return abs(p - o) / abs(o)
 
+def absolute_error(o,p):
+    return abs(o-p)
+
 df_data = pd.read_table(parent+"/data/covid_mixed.txt",delimiter=" ",header=None,skiprows=1,skipinitialspace=True)
 df_sols = pd.read_table(parent+"/output/solutions_covid_mixed.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
 df_optimal_ntrains = pd.read_table(parent+"/output/optimal_ntrains.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
@@ -40,43 +43,44 @@ for i in range(n_pred_days):
     for j in range(5):
         p = models.cubic(x[0],x[1],x[2],tm+j+1,ym,tm)
         o = df_data[0].values[i+j+35]
-        errors[i,j] = relative_error(o,p)
-
+        errors[i,j] = absolute_error(o,p)
 
 print("\nAverages: ")
 
 for i in range(5):
     print("%.4f" % st.mean(errors[:,i]))
 
-successes = np.zeros(5,dtype=int)
 
-for i in range(n_pred_days):
-    for j in range(5):
-        if errors[i,j] <= 0.1:
-            successes[j] += 1
 
-print("\nSuccesses 10%: ")
-print(successes)
+# successes = np.zeros(5,dtype=int)
 
-successes[:] = 0
+# for i in range(n_pred_days):
+#     for j in range(5):
+#         if errors[i,j] <= 0.1:
+#             successes[j] += 1
 
-for i in range(n_pred_days):
-    for j in range(5):
-        if errors[i,j] <= 0.15:
-            successes[j] += 1
+# print("\nSuccesses 10%: ")
+# print(successes)
 
-print("\nSuccesses 15%: ")
-print(successes)
+# successes[:] = 0
 
-successes[:] = 0
+# for i in range(n_pred_days):
+#     for j in range(5):
+#         if errors[i,j] <= 0.15:
+#             successes[j] += 1
 
-for i in range(n_pred_days):
-    for j in range(5):
-        if errors[i,j] <= 0.2:
-            successes[j] += 1
+# print("\nSuccesses 15%: ")
+# print(successes)
 
-print("\nSuccesses 20%: ")
-print(successes)
+# successes[:] = 0
+
+# for i in range(n_pred_days):
+#     for j in range(5):
+#         if errors[i,j] <= 0.2:
+#             successes[j] += 1
+
+# print("\nSuccesses 20%: ")
+# print(successes)
 
 fig, ax = plt.subplots()
 mtrains = df_optimal_ntrains[0].values[:]
