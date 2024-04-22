ea233756ed6c079ab954486c82321f69596bbddb
Gustavo <gustavoquintero9105@gmail.com>
2024-04-22 12:37:48 -0500

    Updated on 04-22-2024 at 12:37

--- experiment_2/sources/data_covid_mixed.py
+++ experiment_2/sources/data_covid_mixed.py
@@ -1,5 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
+
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
 
 countries = {
     "ar" : "ar.xlsx",
@@ -12,7 +20,7 @@ countries = {
     "us" : "us.xlsx"
 }
 
-country = countries["it"]
+country = countries["uk"]
 
 cwd = os.getcwd()
 parent =  os.path.abspath(os.path.join(cwd,os.pardir))
@@ -24,14 +32,24 @@ ind_excel_end   = 1124 # 25-05-2023
 init_date       = ind_excel_init - 2
 end_date        = ind_excel_end - 2
 n = ind_excel_end-ind_excel_init + 1
+data = np.empty(n)
 
 
 with open(parent+"/data/covid_mixed.txt","w") as f:
     f.write("%i\n" % n)
     for i in range(n):
         x = df["new_deaths_smoothed_per_million"][i+init_date]
+        data[i] = x
 
         if pd.isna(x) == True:
             f.write("%f\n" % 0.0)
         else:
             f.write("%f\n" % x)
+
+t = np.linspace(1,139,139)
+
+plt.plot(t,data[:139],color="darkgreen")
+plt.xlabel("Days",fontdict=font)
+plt.ylabel("New deaths per million",fontdict=font)
+plt.savefig(parent+"/images/data.pdf",bbox_inches = "tight")
+plt.show()
