8ef9299cb6ac6cb43581ce97b853b621ce81df27
Gustavo <gustavoquintero9105@gmail.com>
2024-05-01 13:03:54 -0500

    Updated on 05-01-2024 at 13:03

--- experiment_2/sources/plot_covid.py
+++ experiment_2/sources/plot_covid.py
@@ -13,7 +13,25 @@ plt.rcParams.update({'font.size': 13})
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-df_sol = pd.read_table(parent+"/output/plot_covid.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_sol_5 = pd.read_table(parent+"/output/plot_covid_5.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_sol_10 = pd.read_table(parent+"/output/plot_covid_10.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_sol_15 = pd.read_table(parent+"/output/plot_covid_15.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_sol_20 = pd.read_table(parent+"/output/plot_covid_20.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_sol_25 = pd.read_table(parent+"/output/plot_covid_25.txt",delimiter=" ",header=None,skiprows=0,skipinitialspace=True)
+df_data = pd.read_table(parent+"/data/covid_25.txt",delimiter=" ",header=None,skiprows=2,skipinitialspace=True)
 
-plt.plot(df_sol[0].values,df_sol[1].values)
-plt.show()
+t = np.linspace(1,30,30)
+
+plt.plot(df_sol_5[0].values+20,df_sol_5[1].values)
+plt.plot(df_sol_10[0].values+15,df_sol_10[1].values)
+plt.plot(df_sol_15[0].values+10,df_sol_15[1].values)
+plt.plot(df_sol_20[0].values+5,df_sol_20[1].values)
+plt.plot(df_sol_25[0].values,df_sol_25[1].values)
+
+l1 = plt.plot(t[:25],df_data[0].values[:25],"ok")
+plt.setp(l1, 'markersize')
+
+l2 = plt.plot(t[25:],df_data[0].values[25:],"ok",mfc='none',ms=6,marker="s")
+plt.setp(l2, 'markersize')
+plt.savefig(parent+"/images/image.pdf",bbox_inches = "tight")
+# plt.show()
